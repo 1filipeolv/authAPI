09066e17e68c846a84e98261cb44e604d5eaad15
1filipeolv <filipeolv.lima@gmail.com>
2025-08-23 17:24:02 -0300

    1filipeolv

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'frontend/src'),
+      '@/components': path.resolve(__dirname, 'frontend/src/components'),
+      '@/context': path.resolve(__dirname, 'context'),
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
