0396fa3a8108be4c9863369585c3beef3b5de86b
Filipe Lima <filipeolv.lima@gmail.com>
2025-08-26 10:42:28 -0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://authapiback.vercel.app/", 
+  baseURL: "https://authapiback.vercel.app/api", 
 });
 
 export default api;
